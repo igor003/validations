fc0f62dd01ae26b32b02fac253b880ecbf153b78
igor003 <igorio6ka1995@gmail.com>
2021-01-18 21:17:19 +0200

    adds view for device types

--- resources/views/device_types_view.blade.php
+++ resources/views/device_types_view.blade.php
@@ -1,17 +1,29 @@
 @extends('layouts.app')
 
 @section('content')
+
 <div class="container">
     <div class="row justify-content-center">
+        <img height='250' src="img/validation_devices.png" alt="">
+    </div>
+    <div class="row justify-content-center">
         <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Dashboard</div>
-
-                <div class="card-body">
-                Hellow!!!
-                {{$device_types}}
-                </div>
-            </div>
+            <table class="table table-bordered">
+                <thead>
+                    <tr>
+                      <th class='text-center' scope="col">Type</th>
+                      <th class='text-center' scope="col">Periodicity (months)</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @foreach($device_types as $device_type)
+                        <tr>
+                          <td class='text-center'><a href=""><button type="button" class="btn btn-outline-info">{{$device_type->name}}</button></a></td>
+                          <td class='text-center'>{{$device_type->periodicity}}</td>
+                        </tr>
+                    @endforeach
+                 </tbody>
+            </table>
         </div>
     </div>
 </div>
